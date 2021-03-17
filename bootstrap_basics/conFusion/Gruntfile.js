a1095afee2f5c0dd98ffa00c3afc88b706890880
Slackoth <00082318@uca.edu.sv>
2021-03-17 05:49:16 -0600

    Configure distribution scripts with grunt

--- bootstrap_basics/conFusion/Gruntfile.js
+++ bootstrap_basics/conFusion/Gruntfile.js
@@ -5,7 +5,7 @@ const sass = require('node-sass');
 
 module.exports = function(grunt) {
     require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
+    require('jit-grunt')(grunt, { useminPrepare: 'grunt-usemin' });
 
     grunt.initConfig({
         sass: {
@@ -32,9 +32,123 @@ module.exports = function(grunt) {
                     src: ['css/*.css', '*.html', 'js/*.js']
                 }
             }
+        },
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist/'
+                }]
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/*.*'],
+                    dest: 'dist/'
+                }]
+            }
+        },
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,                  // Enable dynamic expansion
+                    cwd: './',                   // Src matches are relative to this path
+                    src: ['img/*.{png,jpg,gif}'],   // Actual patterns to match
+                    dest: 'dist/'                  // Destination path prefix
+                }]
+            }
+        },
+        useminPrepare: {
+            foo: {
+                dest: 'dist/',
+                src: ['contactus.html', 'aboutus.html', 'index.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css: [{
+                            name: 'cssmin',
+                            createConfig: function(context, block) {
+                                var generated = context.options.generated;
+
+                                generated.options = {
+                                    keepSpecialComments: 0, 
+                                    rebase: false
+                                };
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+        uglify: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+        cssmin: {
+            // dist configuration is provided by useminPrepare
+            dist: {}
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 20
+            },
+            // filerev:release hashes(md5) all assets (images, js and css )
+            // in dist directory
+            release: {
+                files: [{
+                    src: ['dist/js/*.js', 'dist/css/*.css']
+                }]
+            }
+        },
+        // Replaces all assets with their revved version in html and css files.
+        // options.assetDirs contains the directories for finding the assets
+        // according to their relative paths
+        usemin: {
+            html: ['dist/contactus.html', 'dist/aboutus.html', 'dist/index.html'],
+            options: {
+                assetsDirs: ['dist', 'dist/css', 'dist/js']
+            },
+        },
+        htmlmin: {
+            dist: {                                        // Target
+                options: {                                 // Target options
+                    collapseWhitespace: true
+                },
+                files: {                                   // Dictionary of files
+                    'dist/index.html': 'dist/index.html',  // 'destination': 'source'
+                    'dist/contactus.html': 'dist/contactus.html',
+                    'dist/aboutus.html': 'dist/aboutus.html',
+                }
+            }
         }
     });
 
     grunt.registerTask('css', ['sass']);
     grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('build', ['clean', 'copy', 'imagemin',
+     'useminPrepare', 'concat', 'cssmin', 'uglify', 'filerev', 'usemin', 'htmlmin']);
 };
